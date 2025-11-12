722eb146562080789e6437f421a5830dd73b657e
Hugo Henrique <hugo.hbs64@gmail.com>
2025-11-12 10:42:17 -0300

    fix(css): ajuste no modal para modo claro e escuro

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const Modal = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="p-8 bg-gray-950 shadow-lg rounded-lg w-full max-w-md">
+    <div className="p-8 bg-gray-50 dark:bg-gray-950 shadow-lg rounded-lg w-full max-w-md">
       {children}
     </div>
   );
