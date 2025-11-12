5f5b09feec9663385a49d8ae305db5b80c520db6
Hugo Henrique <hugo.hbs64@gmail.com>
2025-11-12 08:42:06 -0300

    fix(css): ajuste no css da Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
       <Image
         className="dark:invert"
         src="/next.svg"
