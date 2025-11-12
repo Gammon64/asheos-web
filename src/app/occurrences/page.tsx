3480fac9041fab66cb95630f6ce0b00dd1f65ce4
Hugo Henrique <hugo.hbs64@gmail.com>
2025-11-12 15:35:27 -0300

    feat(occurrence): adicionando link para detalhes

--- src/app/occurrences/page.tsx
+++ src/app/occurrences/page.tsx
@@ -1,3 +1,5 @@
+import H1 from "@/components/H1";
+import Chip from "@/components/occurrences/Chip";
 import { api } from "@/lib/axios";
 import { Occurrence } from "@/types/occurrence";
 import { cookies } from "next/headers";
@@ -26,7 +28,7 @@ const OccurrencePage = async () => {
   return (
     <div className="container max-w-3xl mx-auto p-4">
       <div className="flex justify-between items-center mb-6">
-        <h1 className="text-3xl font-bold">Minhas Ocorrências</h1>
+        <H1>Minhas Ocorrências</H1>
         <Link
           href="/occurrences/create"
           className="bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition"
@@ -43,26 +45,24 @@ const OccurrencePage = async () => {
           {occurrences.map((occ) => (
             <div
               key={occ.id}
-              className="bg-white p-4 rounded-lg shadow border border-gray-200"
+              className="bg-white dark:bg-black p-4 rounded-lg shadow border border-gray-200 dark:border-gray-800"
             >
               <div className="flex justify-between items-center">
-                <h2 className="text-xl font-semibold text-gray-800">
-                  {occ.title}
-                </h2>
-                <span
-                  className={`py-1 px-3 rounded-full text-sm font-medium ${
-                    occ.status === "OPENED"
-                      ? "bg-green-100 text-green-800"
-                      : "bg-gray-100 text-gray-800"
-                  }`}
-                >
-                  {occ.status}
-                </span>
+                <h2 className="text-xl font-semibold">{occ.title}</h2>
+                <Chip status={occ.status} />
               </div>
               <p className="text-gray-600 mt-2">{occ.description}</p>
-              <div className="text-sm text-gray-400 mt-4">
-                Reportado por: {occ.reportedBy.name} em{" "}
-                {new Date(occ.createdAt).toLocaleDateString()}
+              <div className="flex justify-between items-center">
+                <div className="text-sm text-gray-400 mt-4">
+                  Reportado por: {occ.reportedBy.name} em{" "}
+                  {new Date(occ.createdAt).toLocaleDateString()}
+                </div>
+                <Link
+                  href={`/occurrences/${occ.id}`}
+                  className="text-blue-600 hover:underline"
+                >
+                  Detalhes
+                </Link>
               </div>
             </div>
           ))}
