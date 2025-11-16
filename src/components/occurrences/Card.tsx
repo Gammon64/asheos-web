932b79c1a0c74f85b13268ae397d32302e094498
Hugo Henrique <hugo.hbs64@gmail.com>
2025-11-16 14:31:23 -0300

    refactor(css): adicionando props de html div no componente Card

--- src/components/occurrences/Card.tsx
+++ src/components/occurrences/Card.tsx
@@ -1,9 +1,8 @@
-import { ReactNode } from "react";
 
-const Card = ({ children }: { children: ReactNode }) => {
+const Card = (props: React.HtmlHTMLAttributes<HTMLDivElement>) => {
   return (
-    <div className="bg-white dark:bg-black p-6 rounded-lg shadow border border-gray-200 dark:border-gray-800 mb-6">
-      {children}
+    <div className={`${props.className} bg-white dark:bg-black p-6 rounded-lg shadow border border-gray-200 dark:border-gray-800 mb-6`}>
+      {props.children}
     </div>
   );
 };
