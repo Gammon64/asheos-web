dd1b8c7837bcdd626320ddecfb0b910705d6cdc4
Hugo Henrique <hugo.hbs64@gmail.com>
2025-11-27 10:54:34 -0300

    refactor(occurrence): separando form de anexos em outra coluna na page de ocorrência; melhorando o visual do botão de anexo.

--- src/app/occurrences/[id]/UploadAttachmentForm.tsx
+++ src/app/occurrences/[id]/UploadAttachmentForm.tsx
@@ -20,18 +20,27 @@ const UploadAttachmentForm = ({ occurrenceId }: UploadAttachmentFormProps) => {
         initialState
     );
     return (
-        <form action={formAction} className="flex items-end gap-4">
+        <form action={formAction} className="flex flex-col gap-4">
             <div>
                 <Label
-                    htmlFor="status"
+                    htmlFor="file"
+                    className="block rounded border p-4 shadow-sm sm:p-6"
                 >
-                    Selecionar arquivo (Máx 10MB)
+                    <div className="flex items-center justify-center gap-4">
+                        <span className="font-medium" > Selecionar arquivo (Máx 10MB) </span>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 7.5h-.75A2.25 2.25 0 0 0 4.5 9.75v7.5a2.25 2.25 0 0 0 2.25 2.25h7.5a2.25 2.25 0 0 0 2.25-2.25v-7.5a2.25 2.25 0 0 0-2.25-2.25h-.75m0-3-3-3m0 0-3 3m3-3v11.25m6-2.25h.75a2.25 2.25 0 0 1 2.25 2.25v7.5a2.25 2.25 0 0 1-2.25 2.25h-7.5a2.25 2.25 0 0 1-2.25-2.25v-.75"></path>
+                        </svg>
+                    </div>
                 </Label>
+                {/* TODO: preview do arquivo */}
                 <Input
                     id="file"
                     name="file"
                     type="file"
                     required
+                    className="sr-only"
                 />
                 {state.properties?.file &&
                     state.properties.file.errors.map((error) => (
--- src/app/occurrences/[id]/page.tsx
+++ src/app/occurrences/[id]/page.tsx
@@ -35,84 +35,87 @@ const OccurrencePage = async ({ params }: OccurrencePageProps) => {
   if (!occurrence) notFound();
 
   return (
-    <Modal>
-      <div className="mb-4">
-        <Link href="/occurrences" className="text-blue-600 hover:underline">
-          &larr; Voltar para Ocorrências
-        </Link>
-      </div>
-      {/* Cabeçalho */}
-      <Card>
-        <div className="flex justify-between items-start">
-          <H1>{occurrence.title}</H1>
-          <Chip status={occurrence.status} />
+    <div className="flex gap-4">
+      <Modal>
+        <div className="mb-4">
+          <Link href="/occurrences" className="text-blue-600 hover:underline">
+            &larr; Voltar para Ocorrências
+          </Link>
         </div>
-        <div className="text-sm text-gray-500 mt-2">
-          Reportado por:{" "}
-          <span className="font-medium text-gray-700">
-            {occurrence.reportedBy.name}
-          </span>
-        </div>
-        <div className="text-sm text-gray-500">
-          Data:{" "}
-          {new Date(occurrence.createdAt).toLocaleDateString("pt-BR", {
-            day: "2-digit",
-            month: "long",
-            year: "numeric",
-          })}
-        </div>
-      </Card>
+        {/* Cabeçalho */}
+        <Card>
+          <div className="flex justify-between items-start">
+            <H1>{occurrence.title}</H1>
+            <Chip status={occurrence.status} />
+          </div>
+          <div className="text-sm text-gray-500 mt-2">
+            Reportado por:{" "}
+            <span className="font-medium text-gray-700">
+              {occurrence.reportedBy.name}
+            </span>
+          </div>
+          <div className="text-sm text-gray-500">
+            Data:{" "}
+            {new Date(occurrence.createdAt).toLocaleDateString("pt-BR", {
+              day: "2-digit",
+              month: "long",
+              year: "numeric",
+            })}
+          </div>
+        </Card>
 
-      {/* Descrição */}
-      <Card>
-        <h2 className="text-xl font-semibold mb-3">Descrição</h2>
-        <p className="text-gray-700 dark:text-gray-300 whitespace-pre-wrap">
-          {occurrence.description}
-        </p>
-      </Card>
+        {/* Descrição */}
+        <Card>
+          <h2 className="text-xl font-semibold mb-3">Descrição</h2>
+          <p className="text-gray-700 dark:text-gray-300 whitespace-pre-wrap">
+            {occurrence.description}
+          </p>
+        </Card>
 
-      {/* Formulário de Atualização de Status */}
-      <Card>
-        <h2 className="text-xl font-semibold mb-4">Atualizar Status</h2>
-        <UpdateStatusForm
-          occurrenceId={occurrence.id}
-          currentStatus={occurrence.status}
-        />
-      </Card>
+        {/* Formulário de Atualização de Status */}
+        <Card>
+          <h2 className="text-xl font-semibold mb-4">Atualizar Status</h2>
+          <UpdateStatusForm
+            occurrenceId={occurrence.id}
+            currentStatus={occurrence.status}
+          />
+        </Card>
 
-      {/* Seção de Anexos */}
-      <Card className="flex flex-col gap-2">
-        <h2 className="text-xl font-semibold mb-4">Anexos</h2>
+        {/* Painel de ações */}
+        <Card>
+          <h2 className="text-xl font-semibold mb-4">Ações</h2>
+          <DeleteOccurrenceButton occurrenceId={occurrence.id} />
+        </Card>
+      </Modal>
+      <Modal>
+        {/* Seção de Anexos */}
+        <Card className="flex flex-col gap-2">
+          <h2 className="text-xl font-semibold mb-4">Anexos</h2>
 
-        <div className="flex flex-col gap-2">
-          {occurrence.attachments.map((attachment) => (
-            <div key={attachment.id} className="flex flex-col items-center justify-between p-2 max-w-full border rounded-md break-all">
-              <p className="w-full">{attachment.fileName}</p>
-              <div className="flex w-full gap-2">
-                <DownloadAttachmentButton
-                  occurrenceId={occurrence.id}
-                  attachmentId={attachment.id}
-                />
-                <DeleteAttachmentButton
-                  occurrenceId={occurrence.id}
-                  attachmentId={attachment.id}
-                />
+          <div className="flex flex-col gap-2">
+            {occurrence.attachments.map((attachment) => (
+              <div key={attachment.id} className="flex flex-col items-center justify-between p-2 max-w-full border rounded-md break-all">
+                <p className="w-full">{attachment.fileName}</p>
+                <div className="flex w-full gap-2">
+                  <DownloadAttachmentButton
+                    occurrenceId={occurrence.id}
+                    attachmentId={attachment.id}
+                  />
+                  <DeleteAttachmentButton
+                    occurrenceId={occurrence.id}
+                    attachmentId={attachment.id}
+                  />
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
 
-        {/* Formulário de Upload */}
-        <h3 className="text-lg font-semibold mb-3 pt-4 border-t">Novo Anexo</h3>
-        <UploadAttachmentForm occurrenceId={occurrence.id} />
-      </Card>
-
-      {/* Painel de ações */}
-      <Card>
-        <h2 className="text-xl font-semibold mb-4">Ações</h2>
-        <DeleteOccurrenceButton occurrenceId={occurrence.id} />
-      </Card>
-    </Modal>
+          {/* Formulário de Upload */}
+          <h3 className="text-lg font-semibold mb-3 pt-4 border-t">Novo Anexo</h3>
+          <UploadAttachmentForm occurrenceId={occurrence.id} />
+        </Card>
+      </Modal>
+    </div>
   );
 };
 
